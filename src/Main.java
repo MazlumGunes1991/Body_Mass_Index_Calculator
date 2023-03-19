7313b61da9a3f524c0bbd20be9ace3e6705447df
Mazlum Güneş <gunesmazlum1991@gmail.com>
2023-03-19 10:35:34 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        double length, weight, bodyMassIndex;
+
+        System.out.print("Length(m): ");
+        length = input.nextDouble();
+
+        System.out.print("Weight(kg): ");
+        weight = input.nextDouble();
+
+        bodyMassIndex = (weight) / (length * length);
+
+        System.out.print("Body Mass Index: " + bodyMassIndex);
     }
 }
